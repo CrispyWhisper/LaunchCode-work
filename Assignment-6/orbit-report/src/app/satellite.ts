3b97e9295745e8857e549addf2b949ad5a21e5df
Ryan <rcbrooks03@gmail.com>
2021-01-05 19:56:28 -0600

    got up to the search function, haven't yet figured it out

--- Assignment-6/orbit-report/src/app/satellite.ts
+++ Assignment-6/orbit-report/src/app/satellite.ts
@@ -4,7 +4,14 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
-    
+    shouldShowWarning: boolean = false;
+    isSpaceDebris() {
+        if(this.type == "Space Debris" || this.type == "space debris") {
+            console.log("this function ran");
+            console.log(this.type);
+            return this.shouldShowWarning = true;
+        }
+    };
 
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
